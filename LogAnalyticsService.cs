0d4048613ce05c7f654c729bbad4345ce70a70ca
Cedric Blomart <cblomart@gmail.com>
2024-01-27 17:54:02 +0100

    Add logging to log analytics

--- LogAnalyticsService.cs
+++ LogAnalyticsService.cs
@@ -14,6 +14,7 @@ namespace SureStacks.O365Logs2LA {
         private readonly ILogger<LogAnalyticsService> _logger;
         private readonly HttpClient _httpClient;
         private readonly string _logAnalyticsUrl;
+        private readonly bool _debug;
         private readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web){
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
             PropertyNameCaseInsensitive = true
@@ -21,6 +22,11 @@ namespace SureStacks.O365Logs2LA {
 
         public LogAnalyticsService(IManagedIdentityTokenService tokenService, ILogger<LogAnalyticsService> logger, IHttpClientFactory httpClientFactory)
         {
+            // get debug from environment variabl
+            var debug = Environment.GetEnvironmentVariable("Debug");
+            if (debug is not null) {
+                _debug = true;
+            }
             _tokenService = tokenService;
             _logger = logger;
             _httpClient = httpClientFactory.CreateClient();
@@ -42,20 +48,37 @@ namespace SureStacks.O365Logs2LA {
 
             // get token
             string token = await _tokenService.GetToken("monitor.azure.com");
-            // set authorization header
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            // log token if debug
+            if (_debug) _logger.LogInformation($"LA: Token: {token}");
+            
+            // create http request
+            HttpRequestMessage request = new(HttpMethod.Post, _logAnalyticsUrl);
 
+            // set authorization header
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
             // set x-ms-date header
-            _httpClient.DefaultRequestHeaders.Add("x-ms-date", DateTime.UtcNow.ToString("r"));
-            // set log-type header from contentType, replace "." with "_"
-            _httpClient.DefaultRequestHeaders.Add("Log-Type", contentType.Replace(".", "_") + "_CL");
+            request.Headers.Add("x-ms-date", DateTime.UtcNow.ToString("r"));
+            // set log-type header from contentType, replace "." with "_" and add "_CL"
+            request.Headers.Add("Log-Type", contentType.Replace(".", "_") + "_CL");
+            
 
             // create string content from logs
             var content = new StringContent(JsonSerializer.Serialize(logs, _jsonOptions));
+            // log content if debug
+            if (_debug) _logger.LogInformation($"LA: Content: {content}");
+            // set request content
+            request.Content = content;
+            // set content type header
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
-            HttpResponseMessage response = await _httpClient.PostAsync(_logAnalyticsUrl, content);
+
+            HttpResponseMessage response = await _httpClient.SendAsync(request);
 
             if (!response.IsSuccessStatusCode) {
+                // if _debug and response has content, log response content
+                if (_debug && response.Content is not null) {
+                    _logger.LogInformation($"LA: Response content: {await response.Content.ReadAsStringAsync()}");
+                }
                 // if unauthorized invalidate the token
                 if (response.StatusCode == HttpStatusCode.Unauthorized) {
                     await _tokenService.InvalidateToken("monitor.azure.com").ConfigureAwait(false);
